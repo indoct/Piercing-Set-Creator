d49fd17d1703f9cd377430a84cf98ff0f57367a5
syoban <syoban@gmail.com>
2024-02-14 16:26:56 +0000

    styling

--- src/components/PiercingSetBlock.jsx
+++ src/components/PiercingSetBlock.jsx
@@ -6,19 +6,22 @@ export default function PiercingSetBlock({ ...props }) {
       ? ""
       : props.set.map((prc) => {
           return (
-            <Col key={prc.prc_nodeid} lg={2}>
-              <ul>
-                <li>{prc.prc_name}</li>
-                <li>{prc.prc_location}</li>
-                <li>{prc.prc_color}</li>
-              </ul>
+            <Col key={prc.prc_nodeid} lg={2} className="prc-col">
+              <div className="prc-container">
+                <div className="img-dummy"></div>
+                <ul className="prc-stats">
+                  <li>{prc.prc_name}</li>
+                  <li>{prc.prc_location}</li>
+                  <li>{prc.prc_color}</li>
+                </ul>
+              </div>
             </Col>
           );
         });
 
   return (
     <>
-      <h2>{props.set.length != undefined ? props.set[0].pt_setname : ""}</h2>
+      <h4>{props.set.length != undefined ? props.set[0].pt_setname : ""}</h4>
       {prcBlock}
     </>
   );
