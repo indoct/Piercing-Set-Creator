c3d0350bc0debe465c4eed51aa398e2abe598102
syoban <syoban@gmail.com>
2024-02-19 10:31:36 +0000

    change to radio buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,9 +17,9 @@ export default function Home() {
           <h1>Indoct's Piercing Set Creator</h1>
           <p className="warning-txt">
             It is strongly advised to use a desktop or laptop device for this as
-            the page may be incredibly long and probably hard to use on mobile.
-            You will also be copying and pasting the code into your file, and
-            that's also difficult on a phone!
+            the page will be very long and probably hard to use on mobile. You
+            will also be copying and pasting the code into your file, and that's
+            also difficult on a phone!
           </p>
         </Col>
       </Row>
@@ -29,78 +29,76 @@ export default function Home() {
             <h4>Customise Settings</h4>
             <p>Choose which piercings you want to display in the app</p>
             <form>
-              <label for="cars">Piercing Color:</label>
-              <br />
-              <select id="cars" name="cars">
-                <option value="volvo">Silver</option>
-                <option value="saab">Gold</option>
-              </select>
-              <br />
-              <input
-                type="checkbox"
-                id="vehicle1"
-                name="vehicle1"
-                value="Bike"
-              />
-              <label for="vehicle1">
-                I'm using Ghouls Vanilla Piercing Recolors
-              </label>
-              <label for="prc-type">Piercing Color:</label>
-              <br />
-              <select id="prc-type" name="prc-type">
-                <option value="all-prcs">All Piercings</option>
-                <option value="vanilla-basic">Vanilla Basic Only</option>
-                <option value="vanilla-all">Vanilla All Only</option>
-                <option value="mods-only">Mod Piercings Only</option>
-              </select>
+              <fieldset>
+                <legend>Piercing Color:</legend>
+                <input
+                  type="radio"
+                  id="silver"
+                  name="prc_color"
+                  value="Silver"
+                />
+                <label htmlFor="silver"> Silver</label>
+                <br />
+                <input type="radio" id="gold" name="prc_color" value="Gold" />
+                <label htmlFor="gold"> Gold</label>
+                <br />
+                <input
+                  type="radio"
+                  id="other"
+                  name="prc_color"
+                  value="other_color"
+                />
+                <label htmlFor="other">
+                  Other Color (I've installed Ghoul's Vanilla Piercing Recolors
+                  mod)
+                </label>
+                <p className="warning">
+                  The piercing images in the app AND your piercings in-game will
+                  be Silver until you have downloaded and added the correct
+                  Recolor pak from here!
+                </p>
+              </fieldset>
+              <fieldset>
+                <legend>Piercings to Display:</legend>
+                <input
+                  type="radio"
+                  id="all_piercings"
+                  name="prc_display"
+                  value="all_piercings"
+                />
+                <label htmlFor="all_piercings"> All Piercings. </label>
+                <span>
+                  Show me everything: all vanilla piercings (humanoid sets,
+                  barbarian-only sets, Orpheus' piercings, all Mod piercings)
+                </span>
+                <br />
+                <input
+                  type="radio"
+                  id="all_mod"
+                  name="prc_display"
+                  value="all_mod"
+                />
+                <label htmlFor="all_mod"> Only Mod Piercings. </label>
+                <span>
+                  ONLY show me Mod piercings (Subtler Piercings, Trips'
+                  Accessory, Ghouls' Custom Piercings)
+                </span>
+                <br />
+                <input
+                  type="radio"
+                  id="all_vanilla"
+                  name="prc_display"
+                  value="all_vanilla"
+                />
+                <label htmlFor="all_vanilla"> Only Vanilla Piercings. </label>
+                <span>
+                  Show me all vanilla piercings (humanoid sets, barbarian-only
+                  sets, Orpheus' piercings) and NO Mod piercings.
+                </span>
+                <br />
+              </fieldset>
             </form>
           </div>
-        </Col>
-        {/* <Col xs lg="4">
-          <div className="settings-box">
-            <h4>Show me (silver) everything!</h4>
-            <p>
-              Start Set Creator with all silver piercings displayed:
-              <br />
-              vanilla, barbarian, Orpheus, mod
-              <strong>
-                Click the customize box if you want piercings in a color other
-                than silver
-              </strong>
-            </p>
-          </div>
-        </Col> */}
-      </Row>
-      <Row className="justify-content-md-center mt-4">
-        <Col xs lg="8">
-          <p>Please select the settings for the set you want to create:</p>
-          <form>
-            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
-            <label for="vehicle1">Just show me everything!</label>
-            <span>
-              This will start the Piercing Set Creator with no custom settings
-              (all SILVER piercings including vanilla, barbarian, Orpheus and
-              mod piercings will be displayed)
-            </span>
-            <br />
-            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
-            <label for="vehicle1">
-              I'm using Ghouls Vanilla Piercing Recolors
-            </label>
-            <br />
-            <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
-            <br />
-            <label for="cars">What piercings do you want to include:</label>
-            <select id="cars" name="cars">
-              <option value="volvo">Volvo</option>
-              <option value="saab">Saab</option>
-              <option value="fiat">Fiat</option>
-              <option value="audi">Audi</option>
-            </select>
-            <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat" />
-            <label for="vehicle3"> I have a boat</label>
-            <br />
-          </form>
         </Col>
       </Row>
     </Container>
