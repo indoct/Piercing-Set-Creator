478a3c3861ee35519dc3b53a3519fb6644a69df4
syoban <syoban@gmail.com>
2024-02-19 17:39:55 +0000

    settings form continued

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -7,16 +7,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   const [settings, setSettings] = useState({
-    prc_color: "",
-    display: "",
+    prc_color: "silver",
+    prc_display: "all-piercings",
   });
 
-  console.log(settings);
+  const styles = {
+    display: settings.prc_color === "other" ? "block" : "none",
+  };
+
+  function genLink() {
+    const urlBase = "./";
+    const urlPage = settings.prc_display;
+    const urlArg = settings.prc_color === "gold" ? "?=gold" : "";
+    return urlBase + urlPage + urlArg;
+  }
+
+  useEffect(() => {
+    console.log(genLink());
+  }, [settings]);
 
   function handleChange(event) {
-    console.log(event.target);
     const { name, value, type, checked } = event.target;
-    console.log(name);
     setSettings((prevSettings) => {
       return {
         ...prevSettings,
@@ -25,6 +36,7 @@ export default function Home() {
     });
   }
 
+  console.log(settings);
   return (
     <Container>
       <Row className="justify-content-md-center">
@@ -42,10 +54,61 @@ export default function Home() {
         <Col xs lg="7">
           <div className="settings-box">
             <h4>Customise Settings</h4>
-            <p>Choose which piercings you want to display in the app</p>
             <form>
               <fieldset>
-                <legend>Piercing Color:</legend>
+                <legend>Piercings to Display:</legend>
+                <section className="display-options">
+                  <label className="display-option" htmlFor="all-piercings">
+                    <input
+                      type="radio"
+                      id="all-piercings"
+                      name="prc_display"
+                      value="all-piercings"
+                      checked={settings.prc_display === "all-piercings"}
+                      onChange={handleChange}
+                    />
+                    All Piercings
+                    <span className="opt-desc">
+                      Show me everything: all vanilla piercings (humanoid sets,
+                      barbarian-only sets, Orpheus' piercings, all Mod
+                      piercings)
+                    </span>
+                  </label>
+                  <label className="display-option" htmlFor="all-mod">
+                    <input
+                      type="radio"
+                      id="all-mod"
+                      name="prc_display"
+                      value="all-mod"
+                      checked={settings.prc_display === "all-mod"}
+                      onChange={handleChange}
+                    />
+                    Only Mod Piercings
+                    <span className="opt-desc">
+                      ONLY show me Mod piercings (Indoct's Subtler Piercings,
+                      Trips' Accessory Collection, Ghouls' Custom Piercings)
+                    </span>
+                  </label>
+                  <label className="display-option" htmlFor="all-vanilla">
+                    <input
+                      type="radio"
+                      id="all-vanilla"
+                      name="prc_display"
+                      value="all-vanilla"
+                      checked={settings.prc_display === "all-vanilla"}
+                      onChange={handleChange}
+                    />
+                    Only Vanilla Piercings.
+                    <span className="opt-desc">
+                      Show me all vanilla piercings (humanoid sets,
+                      barbarian-only sets, Orpheus' piercings) and NO Mod
+                      piercings.
+                    </span>
+                  </label>
+                </section>
+              </fieldset>
+              <fieldset>
+                <legend>Piercing Metal Color:</legend>
                 <section className="options">
                   <label className="color-option" htmlFor="silver">
                     <div className="silver-gr color"></div>
@@ -54,80 +117,41 @@ export default function Home() {
                       id="silver"
                       name="prc_color"
                       value="silver"
-                      checked={settings.color === "silver"}
+                      checked={settings.prc_color === "silver"}
                       onChange={handleChange}
                     />
                     Silver
                   </label>
-                  <div className="color-option">
+                  <label className="color-option" htmlFor="gold">
                     <div className="gold-gr color"></div>
                     <input
                       type="radio"
                       id="gold"
                       name="prc_color"
                       value="gold"
-                      checked={settings.color === "gold"}
+                      checked={settings.prc_color === "gold"}
                       onChange={handleChange}
                     />
-                    <label htmlFor="gold"> Gold</label>
-                  </div>
-                  <div className="color-option">
+                    Gold
+                  </label>
+                  <label className="color-option" htmlFor="other">
                     <div className="other-gr color">?</div>
                     <input
                       type="radio"
                       id="other"
                       name="prc_color"
                       value="other"
-                      checked={settings.color === "other"}
+                      checked={settings.prc_color === "other"}
                       onChange={handleChange}
                     />
-                    <label htmlFor="other">Other Color</label>
-                  </div>
+                    Other Color
+                  </label>
                 </section>
-                <p className="warning">
+                <p className="warning" style={styles}>
                   WARNING: The piercing images in the app AND your piercings
                   in-game will be Silver until you have downloaded and added the
                   correct Recolor pak from here!
                 </p>
-              </fieldset>
-              <fieldset>
-                <legend>Piercings to Display:</legend>
-                <input
-                  type="radio"
-                  id="all_piercings"
-                  name="prc_display"
-                  value="all_piercings"
-                />
-                <label htmlFor="all_piercings"> All Piercings. </label>
-                <span>
-                  Show me everything: all vanilla piercings (humanoid sets,
-                  barbarian-only sets, Orpheus' piercings, all Mod piercings)
-                </span>
-                <br />
-                <input
-                  type="radio"
-                  id="all_mod"
-                  name="prc_display"
-                  value="all_mod"
-                />
-                <label htmlFor="all_mod"> Only Mod Piercings. </label>
-                <span>
-                  ONLY show me Mod piercings (Subtler Piercings, Trips'
-                  Accessory, Ghouls' Custom Piercings)
-                </span>
-                <br />
-                <input
-                  type="radio"
-                  id="all_vanilla"
-                  name="prc_display"
-                  value="all_vanilla"
-                />
-                <label htmlFor="all_vanilla"> Only Vanilla Piercings. </label>
-                <span>
-                  Show me all vanilla piercings (humanoid sets, barbarian-only
-                  sets, Orpheus' piercings) and NO Mod piercings.
-                </span>
-                <br />
               </fieldset>
             </form>
           </div>
