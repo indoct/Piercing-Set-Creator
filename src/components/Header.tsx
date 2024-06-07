3ff3a2f746f659783d1e261cfb9f0bf54da2d3a4
syoban <syoban@gmail.com>
2024-06-07 20:20:37 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -216,7 +216,7 @@ export default function Header(): JSX.Element {
                   }
                 }}
               >
-                Reset Filters
+                Reset All Filters
               </button>
             )}
           </Col>
