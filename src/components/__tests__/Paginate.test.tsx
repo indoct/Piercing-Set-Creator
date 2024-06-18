c4733a652a7aa471cfa1021c1dae73a06b5c5293
Nathan Yam <nathancyam@gmail.com>
2024-06-18 11:39:05 +1000

    chore: Add more flipped tests

--- src/components/__tests__/Paginate.test.tsx
+++ src/components/__tests__/Paginate.test.tsx
@@ -3,6 +3,8 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import Paginate from '../Paginate';
 import { Piercing } from '../../types';
 
+const emptyCb = () => {}
+
 function createPiercing(p: Partial<Piercing> | ((p: Piercing) => Piercing) = {}): Piercing {
   const defaultPiercing: Piercing = {
     index: 0,
@@ -35,8 +37,8 @@ test('shows loading paragraph when pages is empty', async () => {
     itemsPerPage={10}
     filteredPiercings={[]}
     currentPage={1}
-    handleBtns={() => {}}
-    handlePageChange={() => {}}
+    handleBtns={emptyCb}
+    handlePageChange={emptyCb}
   />);
 
   expect(screen.getByText('Loading...')).toBeDefined();
@@ -47,8 +49,8 @@ test('shows loading paragraph when current page is less than 1', async () => {
     itemsPerPage={10}
     filteredPiercings={[createPiercing()]}
     currentPage={-1}
-    handleBtns={() => {}}
-    handlePageChange={() => {}}
+    handleBtns={emptyCb}
+    handlePageChange={emptyCb}
   />);
 
   expect(screen.getByText('Loading...')).toBeDefined();
@@ -59,8 +61,8 @@ test('shows loading paragraph when current page is greater than the level of pag
     itemsPerPage={10}
     filteredPiercings={[createPiercing()]}
     currentPage={2}
-    handleBtns={() => {}}
-    handlePageChange={() => {}}
+    handleBtns={emptyCb}
+    handlePageChange={emptyCb}
   />);
 
   expect(screen.getByText('Loading...')).toBeDefined();
@@ -77,8 +79,8 @@ test('shows piercings', async () => {
     itemsPerPage={10}
     filteredPiercings={piercings}
     currentPage={1}
-    handleBtns={() => {}}
-    handlePageChange={() => {}}
+    handleBtns={emptyCb}
+    handlePageChange={emptyCb}
   />);
 
   const buttons = await screen.findAllByRole("button")
@@ -97,6 +99,34 @@ test('shows piercings', async () => {
   expect(pageNumbers[1]?.classList.contains('current-page')).toBeFalsy()
 })
 
+test.each([
+  ["piercing_brow_a_l"],
+  ["piercing_lobe_b_l"],
+  ["piercing_tragus_a_l"],
+  ["beard_upper_lip1_l"],
+  ["piercing_brow_b_l"],
+  ["lowerlip_08", "ghouls_customs"],
+])('flips for entries: %s with a category of %s', async (bone: string, category?: string) => {
+  const piercings = Array.from(new Array(20)).map((_, idx) => createPiercing({
+    index: idx,
+    nodeid: `nodeid-${idx}`,
+    bone: idx === 0 ? bone : 'not-bone',
+    ...(category ? { site_cat: category, bone } : {}),
+  }))
+
+  render(<Paginate
+    itemsPerPage={10}
+    filteredPiercings={piercings}
+    currentPage={1}
+    handleBtns={emptyCb}
+    handlePageChange={emptyCb}
+  />);
+
+  const buttons = await screen.findAllByRole("button")
+  // Assert the flipped logic worked with the lobe piercing
+  expect(buttons[0].querySelector('img')?.classList.contains('flipped')).toBeTruthy()
+})
+
 test('calls callback when piercing is clicked', async () => {
   const piercings = Array.from(new Array(20)).map((_, idx) => createPiercing({
     index: idx,
@@ -111,11 +141,14 @@ test('calls callback when piercing is clicked', async () => {
     filteredPiercings={piercings}
     currentPage={1}
     handleBtns={spy}
-    handlePageChange={() => {}}
+    handlePageChange={emptyCb}
   />);
 
   const buttons = await screen.findAllByRole("button")
-  expect(buttons).toHaveLength(12) // Two addtional buttons
+  expect(buttons).toHaveLength(12) // Two addtional buttons for pagination
+
+  const paginationBtns = (await screen.findByTestId("pagination")).querySelectorAll('button')
+  expect(paginationBtns).toHaveLength(2)
 
   fireEvent.click(buttons[0])
   await screen.findAllByRole("button")
@@ -136,7 +169,7 @@ test('calls pagination callback when clicked', async () => {
     itemsPerPage={10}
     filteredPiercings={piercings}
     currentPage={1}
-    handleBtns={() => {}}
+    handleBtns={emptyCb}
     handlePageChange={spy}
   />);
 
@@ -146,4 +179,23 @@ test('calls pagination callback when clicked', async () => {
   await screen.findAllByRole("button")
 
   expect(spy).toHaveBeenCalled()
+})
+
+test('adds one entry to pagination if the list bleeds over', async () => {
+  const piercings = Array.from(new Array(22)).map((_, idx) => createPiercing({
+    index: idx,
+    nodeid: `nodeid-${idx}`,
+    bone: idx === 0 ? 'piercing_lobe_a_l' : 'not-bone',
+  }))
+
+  render(<Paginate
+    itemsPerPage={10}
+    filteredPiercings={piercings}
+    currentPage={1}
+    handleBtns={emptyCb}
+    handlePageChange={emptyCb}
+  />);
+
+  const buttons = (await screen.findByTestId("pagination")).querySelectorAll('button')
+  expect(buttons).toHaveLength(3)
 })
